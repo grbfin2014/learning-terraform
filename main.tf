dddd7f69484672afc8ec4b567e4f7754acdfaddb
grbfin2014 <grbfin2014@yahoo.com>
2025-01-02 15:21:37 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,11 +23,25 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.2.0"
+  name = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingrees_rules       = ["http-80-tc","https-443-tcp"]
+  ingress_cidr_blocks = [0.0.0.0/0]
+
+  egrees_rules       = ["all-all"]
+  engress_cidr_blocks = [0.0.0.0/0]
 }
 
 resource "aws_security_group" "blog" {
