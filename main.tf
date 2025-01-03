73fb6f30bdd664bd0fcf54903757d654070910b0
grbfin2014 <grbfin2014@yahoo.com>
2025-01-02 21:30:07 -0800

    learnung_terraform_reverchanges_autoscaling_01

--- main.tf
+++ main.tf
@@ -34,22 +34,20 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.0.1"
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
-  name = "blog"
-  min_size = 1
-  max_size = 2
-  vpc_zone_identifier     = module.blog_vpc.public_subnets
-  target_group_arns       = module.blog_alb.target_group_arns
-  vpc_security_group_ids  = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  image_id            = data.aws_ami.app_ami.id
-  instance_type       = var.instance_type
+  subnet_id = module.blog_vpc.public_subnets[0]
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
 
-module "blog_alb" {
+module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
